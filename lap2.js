d5cb9f125a93ffe27cdee1a45b0bf922080d7499
Tuannunez <tuantrungpham008@gmail.com>
2025-11-05 13:13:25 +0700

    update

--- lap2.js
+++ lap2.js
@@ -1,7 +1,29 @@
-function varExample(){
-    if(true) {
-        var x = 10;
-    }
-    console.log(x);
+// Bài 1
+for (let i = 0; i < 3; i++){
+    setTimeout(function() {
+        console.log(i);
+    }, 100);
 }
-varExample()
+
+// Bài 2
+const student = {
+    name: "Huyền",
+    age: 19,
+};
+student.grade = "A ";
+student.age = "21";
+console.log(student);
+
+
+//2. Template Literals
+//Bài 1
+const user = {
+    firstName: "Nguyen",
+    lastName: "Van B",
+    product: "IP17",
+    price: "30000000",
+    orderDate: "2025-10-30", 
+};
+const emailTemplate = ` `;
+
+console.log(emailTemplate);
