17d44448d11188fda79b48d9871e662b71add0a3
Tuannunez <tuantrungpham008@gmail.com>
2025-11-12 13:15:37 +0700

    update

--- lap4.js
+++ lap4.js
@@ -19,8 +19,8 @@
 //     callback("loi khong data", null);
 // }
 
-// Bài tập Array Destructuring
-// Bài 1
+// Bài tập:1.Array Destructuring
+// Bài 1.1
 function getFirsLast(array){
   const lfc = array[1]
   array[1] = array[3]
@@ -28,7 +28,7 @@ function getFirsLast(array){
   return array
 }
 console.log(getFirsLast([1,2,3,4,5]));
-// Bài 2 
+// Bài 2.1
 
 function swapElements(arr) {
     // Your code here
@@ -41,8 +41,8 @@ function swapElements(arr) {
 console.log(swapElements([1, 2, 3, 4, 5])); // [1, 4, 3, 2, 5]
 
 
-// Bài tập Object Destructuring
-// Bài 1
+// Bài tập: 2.Object Destructuring
+// Bài 1.2
 const user = {
   id: 1,
   personalInfo: {
@@ -61,3 +61,58 @@ function getUserInfo(user) {
 
 console.log(getUserInfo(user));
 // { name: 'javascript', email: 'javascript@email.com' }
+
+//Bài tập: 3.Promises
+// bài 1.3
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+delay(2000).then(() => console.log("2 seconds passed"));
+
+// bài 2.3
+function fetchMultipleData(urls) {
+  return Promise.all(
+    urls.map((url) => fetch(url).then((res) => res.json()))
+  );
+}
+
+fetchMultipleData(["/api/lfc/1", "/api/sou/2"])
+  .then((users) => console.log(users))
+  .catch((err) => console.error("Lỗi:", err));
+
+
+// Bài tập: 4.Async/Await
+// bài 1.4
+//// Viết lại hàm này sử dụng async/await
+//function processOrder(orderId, callback) {
+  // getOrder(orderId, (order) => {
+  //   getUser(order.userId, (user) => {
+  //     getProducts(order.productIds, (products) => {
+  //       callback({ order, user, products });
+  //     });
+  //   });
+  // });
+//}
+async function processOrder(orderId){
+  const order = await getOrder(orderId);
+  const user = await getUserInfo(order.userId);
+  const products = await getProducts(order.productIds);
+  return { order, user, products };
+}
+
+//bài 2.4
+//  async function safeApiCall(apiFunction, ...args) {
+  // Your code here
+// }
+
+async function  safeApiCall(apiFunction, ...args) {
+  try{
+    const result = await apiFunction(...args);
+    return {data: result, console, error: null};
+  } catch (error){
+    return {data: null, error: error.message || error};
+  }
+}
